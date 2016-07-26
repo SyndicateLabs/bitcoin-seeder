13b80360de92110151b41b0f262695396cec91d6
development@syndicatelabs.org <hardyahrhar11>
2016-07-25 22:30:16 -0700

    Changes for Syndicate

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0xd2, 0x2d, 0x1c, 0xe5 };
 
 CMessageHeader::CMessageHeader()
 {
